152ae735033891910fdf58840a397e7c84ca9f6f
FredyK200 <qwertygeek01@gmail.com>
2021-03-10 12:22:18 -0500

    Added project2.win
    Created a small side project to see how well the program would run natively on my machine instead of through the books terrible VM. professor was impressed

--- project2_win.c
+++ project2_win.c
@@ -1,4 +1,4 @@
-//Project2.c - Multi-threaded Hybrid Quicksort
+//Project2.c - Multi-threaded Hybrid hybridsort
 //
 //Author: Kane Eder
 //  Date: March 4, 2021
@@ -8,19 +8,17 @@
 //			It uses a multi-threaded, hybridized quicksort implementation, which switches
 //			from quicksort to Shell sort when the segment size is below a given threashold
 //
-#define _GNU_SOURCE
+//This version was built for fun to compare and contrast linux vs Windows thread implementations
 
+#include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <errno.h>
-#include <sys/time.h>
-#include <unistd.h>
+#include <time.h>
 #include <stdbool.h>
 
 int *LIST; 
 int THRESHOLD;
-int SIZE;
+int ARRAYSIZE;
 int SEED = 0;
 typedef struct 
 {
@@ -28,8 +26,9 @@ typedef struct
 	int hi;
 	int size;
 } param;
-
-void *quicksort(void *threadarg); 
+DWORD WINAPI sort(LPVOID threadarg);
+double getTime();
+void hybridsort(int lo, int hi, int size); 
 int partition(int lo, int hi);
 bool isSorted();
 
@@ -41,15 +40,14 @@ int main(int argc, char *argv[])
             scramble_start, scramble_end,
 			sort_cpu_s, sort_cpu_e,
 			partition_start, partition_end;
-	struct timeval 	wall_start, wall_end,
-					sort_wall_s, sort_wall_e;
+
 	double 	create_time, initialize_time, scramble_time,
 			partition_time, 
+			win_cpu_start, win_cpu_end,
 			sort_wall_total, sort_cpu_total, wall_total, cpu_total;
 	
 	cpu_start = clock();
-	gettimeofday(&wall_start, NULL);
-	
+
 	bool multithread = true;
 	int pieces = 9;
 	int maxthreads = 4;	
@@ -60,6 +58,7 @@ int main(int argc, char *argv[])
 	{
 		case 7:	// Set # of Threads
 			maxthreads = atoi(argv[6]);
+			if(maxthreads < 0) {return -1;}
 		case 6: // Set # of Pieces
 			pieces = atoi(argv[5])-1;
 			if (pieces < maxthreads) {return -1;}
@@ -79,11 +78,11 @@ int main(int argc, char *argv[])
 			}
 		case 3: // Set size & threshold
 			THRESHOLD = atoi(argv[2]);
-			SIZE = atoi(argv[1]);
-			if (pieces >= SIZE) {return -1;}
+			ARRAYSIZE = atoi(argv[1]);
+			if (pieces >= ARRAYSIZE) {return -1;}
 			break;
 		default:	// Print Error if Wrong Number of Arguments
-			printf("usage: ./project2 SIZE THRESHOLD [SEED [MULTITHREAD [PIECES [THREADS]]]]\n");
+			printf("usage: ./project2 ARRAYSIZE THRESHOLD [SEED [MULTITHREAD [PIECES [THREADS]]]]\n");
 			return -1;
 	}
 	/* ==================================
@@ -91,21 +90,21 @@ int main(int argc, char *argv[])
 	 ==================================*/
 		// Create Array
 	create_start = clock();
-	LIST = (int *)malloc(SIZE * sizeof(int));
+	LIST = (int *)malloc(ARRAYSIZE * sizeof(int));
 	create_end = clock();
 	create_time = ((double)create_end - (double)create_start)/CLOCKS_PER_SEC;
 		// Initialize Array
 	initialize_start = clock();
-	for (int i = 0; i < SIZE; i++) { 
+	for (int i = 0; i < ARRAYSIZE; i++) { 
 		*(LIST + i) = i; 
 	}
 	initialize_end = clock();
 	initialize_time = ((double)initialize_end - (double)initialize_start)/CLOCKS_PER_SEC;
 		// Scramble Array
 	scramble_start= clock();
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < ARRAYSIZE; i++)
         {
-            int r = rand() % SIZE;
+            int r = rand() % ARRAYSIZE;
        		int temp = *(LIST+i);
        		*(LIST+i) = *(LIST+r);
        		*(LIST+r) = temp;
@@ -114,14 +113,14 @@ int main(int argc, char *argv[])
 	scramble_time = ((double)scramble_end - (double)scramble_start)/CLOCKS_PER_SEC;
 
 	cpu_start = clock();
-	gettimeofday(&sort_wall_s, NULL);
+	win_cpu_start = getTime();
+	sort_cpu_s = clock();
 	partition_start = clock();
-	
 	if(multithread)
 	{
 	/* ==================================
                  Partition Segments
-	==================================*/
+    ==================================*/
 
 	param *parts= (param *) malloc(pieces * sizeof(param));
 	int lo, hi, size, i, count = 0;
@@ -129,8 +128,8 @@ int main(int argc, char *argv[])
 	param *right = (param *) malloc(sizeof(param));
 
 	(parts + count)->lo = 0;		// L
-	(parts + count)->hi = SIZE-1; 	// R
-	(parts + count)->size = SIZE;	// part size
+	(parts + count)->hi = ARRAYSIZE-1; 	// R
+	(parts + count)->size = ARRAYSIZE;	// part size
 		while(count < pieces)
 		{
 			lo = (parts + count)->lo;
@@ -167,59 +166,70 @@ int main(int argc, char *argv[])
 	 		/* ==================================
 	                   Create Threads
 	         ==================================*/
-		int ret_val;		
-		pthread_t tid[maxthreads];	
-		pthread_attr_t attr[maxthreads];
-		
-		for (int i=0; i < maxthreads; i++) 
-		{	
-			pthread_attr_init(&attr[i]); 
-			printf("(%9i, %9i, %9i)\n",(parts+count)->lo, (parts+count)->hi, (parts+count)->size);   
-			pthread_create(&tid[i], &attr[i], quicksort, (parts + count)); 
-			count--;
-		}	
-		while(count >= 0)
+		int ret_val;
+		DWORD *ThreadId;
+		HANDLE *ThreadHandle;
+		ThreadId = (DWORD*)malloc(maxthreads * sizeof(DWORD));
+		ThreadHandle = (HANDLE*)malloc(maxthreads * sizeof(HANDLE));
+		for (int i = 0; i < 4; i++)
 		{
-			for(int i=0; i < maxthreads; i++)
+			printf("(%9i, %9i, %9i)\n", (parts + count)->lo, (parts + count)->hi, (parts + count)->size);
+			*(ThreadHandle + i) = CreateThread(
+				NULL,				/* default security attributes */
+				0,					/* default stack aSIZE */
+				sort,			/* thread function */
+				(parts + count),	/* parameter to thread function */
+				0,					/* default creation flags */
+				(ThreadId + i));		/* returns the thread identifier */
+			count--;
+		}
+		while (count >= 0)
+		{
+			for (int i = 0; i < maxthreads; i++)
 			{
-				ret_val = pthread_tryjoin_np(tid[i],NULL); 
-				if (ret_val == 0 && count >= 0) 
+				ret_val = WaitForSingleObject(ThreadHandle[i], 0);
+				if (ret_val == WAIT_OBJECT_0 && count >= 0)
 				{
-					printf("(%9i, %9i, %9i)\n",(parts+count)->lo, (parts+count)->hi, (parts+count)->size);   
-					pthread_create(&tid[i], &attr[i], quicksort, (parts+count));
+					printf("(%9i, %9i, %9i)\n", (parts + count)->lo, (parts + count)->hi, (parts + count)->size);
+					*(ThreadHandle + i) = CreateThread(
+						NULL,				/* default security attributes */
+						0,					/* default stack aSIZE */
+						sort,			/* thread function */
+						(parts + count),	/* parameter to thread function */
+						0,					/* default creation flags */
+						(ThreadId + i));		/* returns the thread identifier */
 					count--;
 				}
 			}
-			usleep(5000);
+			Sleep(5000);
 		}
-	
+
 		for (int i = 0; i < maxthreads; i++)
-		{			
-			pthread_join(tid[i], NULL);
+		{
+			WaitForMultipleObjects(4, ThreadHandle, TRUE, INFINITE);
 		}
 	}
 	else
 	{
 		partition_time = 0;
-		param *parts= (param *) malloc(pieces * sizeof(param));
-		(parts)->lo = 0;		// L
-		(parts)->hi = SIZE-1; 	// R
-		(parts)->size = SIZE;	// part size
-		quicksort(parts);
+		hybridsort(0, ARRAYSIZE-1, ARRAYSIZE);
 	}
-	sort_cpu_e = clock();
-	gettimeofday(&sort_wall_e, NULL);
 
-		/* ==================================
-                    Finish Up
-         ==================================*/
-	sort_wall_total = ((double)sort_wall_e.tv_sec-(double)sort_wall_s.tv_sec) + ((double)sort_wall_e.tv_usec-(double)sort_wall_s.tv_usec)/1000000;
-	sort_cpu_total = ((double)sort_cpu_e - (double)sort_cpu_s)/CLOCKS_PER_SEC;
-	wall_total = ((double)sort_wall_e.tv_sec-(double)wall_start.tv_sec) + ((double)sort_wall_e.tv_usec-(double)wall_start.tv_usec)/1000000;
-	cpu_total = ((double)sort_cpu_e - (double)cpu_start)/CLOCKS_PER_SEC;
-	printf("    SIZE    THRESHOLD SD PC T CREATE   INIT  SHUFFLE   PART  SrtWall Srt CPU ALLWall ALL CPU\n");
+/* ==================================
+			Finish Up
+ ==================================*/
+	sort_cpu_e = clock();
+	win_cpu_end = getTime();
+	wall_total = ((double)sort_cpu_e - (double)cpu_start) / CLOCKS_PER_SEC;
+	sort_wall_total = ((double)sort_cpu_e - (double)sort_cpu_s) / CLOCKS_PER_SEC;
+	sort_cpu_total =  (win_cpu_end - win_cpu_start);
+	cpu_total = (win_cpu_end - (double)cpu_start) / CLOCKS_PER_SEC;
+
+
+
+	printf("  ARRAYSIZE THRESHOLD SD PC T CREATE   INIT  SHUFFLE   PART  SrtWall Srt CPU ALLWall ALL CPU\n");
 	printf("  --------- --------- -- -- - ------ ------- ------- ------- ------- ------- ------- -------\n");
-	printf("F:%9d %9d %02d %2d %1d %0.3f  %0.3f   %0.3f   %0.3f   %0.3f   %0.3f   %0.3f   %0.3f\n",SIZE,THRESHOLD, SEED, pieces + 1, maxthreads, create_time, initialize_time, scramble_time, partition_time, sort_wall_total, sort_cpu_total, wall_total, cpu_total);
+	printf("F:%9d %9d %2d %2d %1d %0.3f  %0.3f   %0.3f   %0.3f   %0.3f   %0.3f   %0.2f   %0.2f\n",ARRAYSIZE,THRESHOLD, SEED, pieces+1, maxthreads, create_time, initialize_time, scramble_time, partition_time, sort_wall_total, sort_cpu_total, wall_total, cpu_total);
 	
 	bool sorted = isSorted();
 	if (!sorted)
@@ -231,20 +241,25 @@ int main(int argc, char *argv[])
 
 /* ==================================
       	Thread Function
-==================================*/	
-void *quicksort(void *threadarg)
+==================================*/
+DWORD WINAPI sort(LPVOID threadarg)
 {
-  	param *part = threadarg;
-    param *left = (param *) malloc(sizeof(param));
-	param *right = (param *) malloc(sizeof(param));
+	param* part = threadarg;
 	int lo = part->lo;
 	int hi = part->hi;
 	int size = part->size;
+	hybridsort(lo, hi, size);
+	return 0;
+}
+
+void hybridsort(int lo, int hi, int size)
+{
+	int left_size, right_size;
 	if (size < 1)
 	{
-		return NULL;
+		return;
 	}
-	else if (size == 1)
+    else if (size == 1)
     {	// COMPARE & SWAP
     	if (*(LIST+lo) > *(LIST+hi))
 		{
@@ -252,12 +267,12 @@ void *quicksort(void *threadarg)
         	*(LIST+lo) = *(LIST+hi);
         	*(LIST+hi) = temp;
 		}
-       	return NULL;
+       	return;
     }
-	else if (size <= THRESHOLD)	
+    else if (size <= THRESHOLD)	
 	{	// SHELL SORT
 		int k=1;
-		while(k <= size+1){k *= 2;} k = (k / 2) - 1;
+		while (k <= size + 1) { k *= 2;} k = (k / 2) - 1;
 		do{
        		for (int i=lo; i < (hi+1-k); i++)
           	{
@@ -265,10 +280,10 @@ void *quicksort(void *threadarg)
                 {
                    	if (*(LIST+j) <= *(LIST+j+k))
 					{
-						break;	
+						break;
 					}
-					else
-					{
+              		else
+					{ 
         				int temp = *(LIST+j);
         				*(LIST+j) = *(LIST+j+k);
         				*(LIST+j+k) = temp;
@@ -277,60 +292,64 @@ void *quicksort(void *threadarg)
             }
             k = k >> 1;
         }while(k > 0); 
-		return NULL;
+		return;
 	}
-	else if (lo < hi)
-	{	// QUICKSORT
+    else if (lo < hi) 
+	{	// hybridsort
 		int p = partition(lo,hi);
-		left->lo = lo;
-		left->hi = p-1;
-		left->size = (p-1) - lo;
-		right->lo = p+1;
-		right->hi = hi;
-		right->size = hi - (p+1);
-		if (left->size < right->size) 
+		left_size = (p-1) - lo;
+		right_size = hi - (p+1);
+		if (left_size < right_size) 
                 {	
-                    quicksort(left);
-    	            quicksort(right);
+                    hybridsort(lo, p-1, left_size);
+    	            hybridsort(p+1, hi, right_size);
                 }
                 else
                 {
-        	 	   quicksort(right);
-                   quicksort(left);
+        	 	   hybridsort(p+1, hi, right_size);
+                   hybridsort(lo, p-1, left_size);
                 }
-		return NULL;
+		return;
 	}
 }
 
 int partition(int lo, int hi)
 {
-	int i = lo, j = hi+1;
-	int x = *(LIST+lo);
-	do{
-		do i++; while (*(LIST+i) < x && i < hi);
-        do j--; while (*(LIST+j) > x && j > lo);
-        if (i < j)	
+        int i = lo, j = hi+1;
+        int x = *(LIST+lo);
+        do{
+                do i++; while (*(LIST+i) < x && i < hi);
+                do j--; while (*(LIST+j) > x && j > lo);
+                if (i < j)
 		{
-			int temp = *(LIST+i);
-		    *(LIST+i) = *(LIST+j);
-   			*(LIST+j) = temp;
+		        int temp = *(LIST+i);
+		        *(LIST+i) = *(LIST+j);
+		        *(LIST+j) = temp;
 		}
-		else{break;}
-	}while(true);
+                else{break;}
+        }while(true);
 	int temp = *(LIST+lo);
 	*(LIST+lo) = *(LIST+j);
 	*(LIST+j) = temp;
-    return j;
+        return j;
 }
 
 bool isSorted()
 {
-    for(int i=0; i < SIZE-1;i++)
-    {
-        if(*(LIST+i+1) != *(LIST+i) + 1)
-		{
-			return false;
-		}
-    }
-	return true;
+        for(int i=0; i < ARRAYSIZE-1;i++)
+        {
+                if(*(LIST+i) != i)
+				{
+					return false;
+				}
+        }
+        return true;
+}
+
+double getTime() 
+{
+	LARGE_INTEGER freq, val;
+	QueryPerformanceFrequency(&freq);
+	QueryPerformanceCounter(&val);
+	return (double)val.QuadPart / (double)freq.QuadPart;
 }
